36e6e414f7e9b14d669c62dfbe2ee1d92f54b3a3
Achraf Jeday <achraf.jday@gmail.com>
2015-12-18 12:20:30 +0100

    CIVIC_673_dkan_feedback_tests

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -1,147 +1,246 @@
 <?php
 
-use Drupal\DKANExtension\Context\RawDKANEntityContext;
-use Drupal\DKANExtension\Context\ModeratorTrait;
-use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Symfony\Component\Console\Helper\Table;
+use Drupal\DKANExtension\Context\RawDKANContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends RawDKANEntityContext {
+class FeatureContext extends RawDKANContext {
 
-  use ModeratorTrait;
+  // /******************************
+  //  * HOOKS
+  //  ******************************/
 
   /**
-   * Initializes context.
+   * @BeforeScenario @disablecaptcha
+   */
+  public function beforeCaptcha()
+  {
+    // Need to both disable the validation function for the captcha
+    // AND disable the appearence of the captcha form field
+    module_load_include('inc', 'captcha', 'captcha');
+    variable_set('disable_captcha', TRUE);
+    captcha_set_form_id_setting('user_login', 'none');
+    captcha_set_form_id_setting('feedback_node_form', 'none');
+    captcha_set_form_id_setting('comment_node_feedback_form', 'none');
+  }
+
+  /**
+   * @AfterScenario @disablecaptcha
+   */
+  public function afterCaptcha()
+  {
+    module_load_include('inc', 'captcha', 'captcha');
+    variable_set('disable_captcha', FALSE);
+    captcha_set_form_id_setting('user_login', 'default');
+    captcha_set_form_id_setting('feedback_node_form', 'default');
+    captcha_set_form_id_setting('comment_node_feedback_form', 'default');
+  }
+
+  // /****************************
+  //  * HELPER FUNCTIONS
+  //  ****************************/
+
+  function getRegion($region) {
+    $session = $this->getSession();
+    $regionObj = $session->getPage()->find('region', $region);
+    if (!$regionObj) {
+      throw new \Exception(sprintf('No region "%s" found on the page %s.', $region, $session->getCurrentUrl()));
+    }
+    return $regionObj;
+  }
+
+  // /*****************************
+  //  * CUSTOM STEPS
+  //  *****************************/
+
+  /**
+   * @Given I should see :number items of :item in the :region region
+   */
+  public function iShouldSeeItemsOfInTheRegion($number, $item, $region) {
+    $regionObj = $this->getRegion($region);
+    // Count the number of items in the region
+    $count = count($regionObj->findAll('css', $item));
+    if (!$count) {
+      throw new \Exception(sprintf("No items found in the '%s' region.", $region));
+    }
+    else {
+      if ($count != $number) {
+        if ($count > $number) {
+          throw new \Exception(sprintf("More than %s items were found in the '%s' region (%s).", $number, $region, $count));
+        }
+        else {
+          throw new \Exception(sprintf("Less than %s items were found in the '%s' region (%s).", $number, $region, $count));
+        }
+      }
+    }
+  }
+
+  /**
+   * @Given I should see :number items of :item or more in the :region region
+   */
+  public function iShouldSeeItemsOfOrMoreInTheRegion($number, $item, $region) {
+    $regionObj = $this->getRegion($region);
+    // Count the number of items in the region
+    $count = count($regionObj->findAll('css', $item));
+    if (!$count) {
+      throw new \Exception(sprintf("No items found in the '%s' region.", $region));
+    }
+    else {
+      if ($count < $number) {
+        throw new \Exception(sprintf("Less than %s items were found in the '%s' region (%s).", $number, $region, $count));
+      }
+    }
+  }
+
+  /**
+   * @Then I should see :arg1 field
+   */
+  public function iShouldSeeField($arg1)
+  {
+    $session = $this->getSession();
+    $page = $session->getPage();
+    $field = $page->findField($arg1);
+    if (!isset($field)) {
+      throw new \Exception(sprintf("Field with the text '%s' not found", $arg1));
+    }
+  }
+
+  /**
+   * @Then I should not see :arg1 field
+   */
+  public function iShouldNotSeeField($arg1)
+  {
+    $session = $this->getSession();
+    $page = $session->getPage();
+    $field = $page->findField($arg1);
+    if ($field) {
+      throw new \Exception(sprintf("Field with the text '%s' is found", $arg1));
+    }
+  }
+
+  /**
+   * @Then the text :text should be visible in the element :element
+   */
+  public function theTextShouldBeVisible($text, $selector)
+  {
+    $element = $this->getSession()->getPage();
+    $nodes = $element->findAll('css', $selector . ":contains('" . $text . "')");
+    foreach ($nodes as $node) {
+      if ($node->isVisible() === TRUE) {
+        return;
+      }
+      else {
+        throw new \Exception("Form item \"$selector\" with label \"$text\" is not visible.");
+      }
+    }
+    throw new \Exception("Form item \"$selector\" with label \"$text\" not found.");
+  }
+
+  /**
+   * @Then the text :text should not be visible in the element :element
+   */
+  public function theTextShouldNotBeVisible($text, $selector)
+  {
+    $element = $this->getSession()->getPage();
+    $nodes = $element->findAll('css', $selector . ":contains('" . $text . "')");
+    foreach ($nodes as $node) {
+      if ($node->isVisible() === FALSE) {
+        return;
+      }
+      else {
+        throw new \Exception("Form item \"$selector\" with label \"$text\" is visible.");
+      }
+    }
+    throw new \Exception("Form item \"$selector\" with label \"$text\" not found.");
+  }
+
+  /**
+   * @Then I should not see the button :button
+   * @Then I should not see the :button button
+   */
+  public function assertButton($button) {
+    $element = $this->getSession()->getPage();
+    $buttonObj = $element->findButton($button);
+    if (!empty($buttonObj)) {
+      throw new \Exception(sprintf("The button '%s' was found on the page %s", $button, $this->getSession()->getCurrentUrl()));
+    }
+  }
+
+  /**
+   * Check items count in workbench tree.
    *
-   * Every scenario gets its own context instance.
-   * You can also pass arbitrary arguments to the
-   * context constructor through behat.yml.
+   * @Given the workbench tree should contain :count elements or more
    */
-  public function __construct() {
-    parent::__construct(
-      'node',
-      'feedback',
-      NULL,
-      array('rating', 'moderation', 'moderation_date')
-      );
-  }
-
-  /**
-   * Creates feedback from a table.
-   *
-   * @Given feedback:
-   */
-  public function addFeedbacks(TableNode $feedbacksTable) {
-    parent::addMultipleFromTable($feedbacksTable);
-  }
-
-  /**
-   * @Then The feedback :title is in :state moderation state
-   */
-  public function theFeedbackIsInModerationState($title, $state) {
-    $node = reset($this->getNodeByTitle($title));
-    if(!$node) {
-      throw new \Exception(sprintf($title . " node not found."));
+  public function theWorkbenchTreeShouldContainCountElements($count)
+  {
+    $session = $this->getSession();
+    $page = $session->getPage();
+    $workflow_list = $page->find('css', 'ul.views-workflow-list');
+    $workflow_list_items = array();
+    // Check if any workflow list is found.
+    if(isset($workflow_list)) {
+      $workflow_list_items = $workflow_list->findAll('css', '.views-workflow-list-title');
     }
-    $this->isNodeInModerationState($node, $state);
-  }
-
-  /**
-   * @Then I vote up the feedback :title
-   */
-  public function iVoteUpTheFeedback($title) {
-    $link = $this->getVotingLink($title, 'fa-angle-up');
-    $link->click();
-  }
-
-  /**
-   * @Then I vote down the feedback :title
-   */
-  public function iVoteDownTheFeedback($title) {
-    $link = $this->getVotingLink($title, 'fa-angle-down');
-    $link->click();
-  }
-
-  private function getVotingLink($title, $link_class) {
-    $links = $this->getSession()->getPage()->findAll('xpath', "//td[contains(@class,'views-field-title')]/a[text()='" . $title . "']/../../td/div/a[contains(@class, '" . $link_class . "')]");
-    return array_pop($links);
-  }
-
-  /**
-   * @Then The feedback :title should be rated :rating
-   */
-  public function theFeedbackIsRated($title, $rating) {
-    $widgets = $this->getSession()->getPage()->findAll('xpath', "//td[contains(@class,'views-field-title')]/a[text()='" . $title . "']/../../td/div/div[contains(@class, 'rate-feedback-rating')]");
-    $widget = array_pop($widgets);
-    $actual = $widget->gettext();
-    if ($actual != $rating) {
-      throw new \Exception("Feedback was rated $actual instead of $rating");
+    if(count($workflow_list_items) < $count) {
+      throw new Exception(sprintf("Workbench tree elements count is different then count provided!"));
     }
   }
 
   /**
-   * @Then I should see a badge next to feedback :title
+   * @Then the :selector elements should be sorted in this order :order
    */
-  public function iShouldSeeABadgeNextToFeedback($title) {
-    $badged_imgs = $this->getSession()->getPage()->findAll('xpath', "//td[contains(@class,'views-field-title')]/a[text()='" . $title . "']/../../td[contains(@class, 'authenticated-user')]");
-    if (empty($badged_imgs)) {
-      throw new \Exception("Feedback '$title' has no badge on the author picture.");
+  public function theElementsShouldBeSortedInThisOrder($selector, $order)
+  {
+    $region = $this->getRegion("content");
+    $items = $region->findAll('css', $selector);
+    $actual_order = array();
+    foreach ($items as $item) {
+      if ($item->getText() !== "") {
+        $actual_order[] = $item->getText();
+      }
+    }
+    $order = explode(" > ", $order);
+    if ($order !== $actual_order) {
+      throw new Exception(sprintf("The elements were not sorted in the order provided."));
     }
   }
 
   /**
-   * @Then I should not see a badge next to feedback :title
+   * @Then I visit the link :selector
    */
-  public function iShouldNotSeeABadgeNextToFeedback($title) {
-    $badged_imgs = $this->getSession()->getPage()->findAll('xpath', "//td[contains(@class,'views-field-title')]/a[text()='" . $title . "']/../../td[contains(@class, 'authenticated-user')]");
-    if (!empty($badged_imgs)) {
-      throw new \Exception("Feedback '$title' has a badge on the author picture.");
+  public function iVisitTheLink($selector)
+  {
+    $region = $this->getRegion("content");
+    $items = $region->findAll('css', $selector);
+    if (!empty($items)) {
+      throw new \Exception("Link '$selector' not found on the page.");
     }
+    $url = reset($items)->getAttribute('href');
+    $session = $this->getSession();
+    $session->visit($this->locatePath($url));
   }
 
   /**
-   * Override RawDKANEntityContext::save()
+   * @Then I click the :link next to :title
    */
-  public function save($fields) {
-    global $user;
-    $username = $fields['author'];
-    $author = user_load_by_name($username);
-
-    if ($author) {
-      $current_user = $user;
-      $user = $author;
+  public function iClickTheLinkNextToTitle($link, $title) {
+    $items = $this->getSession()->getPage()->findAll('xpath', "//span[contains(@class,'views-dkan-workflow-tree-title')]/a[text()='" . "[Feedback] " . $title . "' or text()='" . "[Dataset] " . $title . "']/../../span[contains(@class, 'views-dkan-workflow-tree-action')]/a[text()='" . $link . "']");
+    if (empty($items)) {
+      throw new \Exception("Link '$link' not found on the page.");
     }
-
-    parent::save($fields);
-
-    if ($author) {
-    // Restore the current behat user.
-      $user = $current_user;
-    }
+    $url = reset($items)->getAttribute('href');
+    $session = $this->getSession();
+    $session->visit($this->locatePath($url));
   }
 
   /**
-   * Override RawDKANEntityContext::post_save()
+   * @Given /^I wait for "([^"]*)" seconds$/
    */
-  public function post_save($wrapper, $fields) {
-    parent::post_save($wrapper, $fields);
-    $this->moderate($wrapper, $fields);
-
-    if (isset($fields['rating'])) {
-      $votes = array(
-        'entity_type' => 'node',
-        'entity_id' => $wrapper->value()->nid,
-        'value_type' => 'points',
-        'value' => $fields['rating'],
-        'tag' => 'feedback',
-        );
-      $criteria = NULL;
-      votingapi_set_votes($votes, $criteria);
-    }
+  public function iWaitForSeconds($seconds)
+  {
+    $session = $this->getSession();
+    $session->wait($seconds * 1000);
   }
+
 }
